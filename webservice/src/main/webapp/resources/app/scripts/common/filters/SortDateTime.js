392a98cf400fbbdca197caa692337cb9296f86df
PhoPhaoHoa <mailien.bk@gmail.com>
2016-04-20 13:55:04 +0700

    filter sort dateTime

--- webservice/src/main/webapp/resources/app/scripts/common/filters/SortDateTime.js
+++ webservice/src/main/webapp/resources/app/scripts/common/filters/SortDateTime.js
@@ -5,12 +5,12 @@ angular.module('appDATN.common')
         var EQUAL = 1;
         var SMALLER = 2;
 
-        convertToyyyymmdd = function(dateTime){
+        convertToyyyymmdd = function (dateTime) {
             var arrDateTime = dateTime.split(' ');
             var arrDate = arrDateTime[0].split('/');
             var out = "";
 
-            for(var i = arrDate.length - 1; i> 0; i--){
+            for (var i = arrDate.length - 1; i > 0; i--) {
                 out = out + arrDate[i];
                 out = out + "/";
             }
@@ -20,23 +20,22 @@ angular.module('appDATN.common')
             return out;
         };
 
-        //If(dateTime1 >= dateTime2) true;
         compareDate = function (dateTime1, dateTime2) {
 
             var dt1 = Date.parse(convertToyyyymmdd(dateTime1));
             var dt2 = Date.parse(convertToyyyymmdd(dateTime2));
 
-            if(dt1 > dt2)
+            if (dt1 > dt2)
                 return LARGER;
 
-            if(dt1 == dt2)
+            if (dt1 == dt2)
                 return EQUAL;
 
-            if(dt1 < dt2)
+            if (dt1 < dt2)
                 return SMALLER;
         };
 
-        //If(dateTime1 >= dateTime2) true;
+        /* If(dateTime1 >= dateTime2) true; */
         compareDateTimes = function (dateTimes1, dateTimes2) {
             var starts = [];
             var ends = [];
@@ -52,13 +51,13 @@ angular.module('appDATN.common')
             ends.push(arrDateTime[1]);
 
             temp = compareDate(starts[0], starts[1]);
-            if(temp == LARGER){
+            if (temp == LARGER) {
                 return true;
             }
 
-            if(temp == EQUAL){
+            if (temp == EQUAL) {
                 temp = compareDate(ends[0], ends[1]);
-                if(temp != SMALLER)
+                if (temp != SMALLER)
                     return true;
             }
 
@@ -68,31 +67,40 @@ angular.module('appDATN.common')
         addLastPosition = function (item, list) {
             list.push(item);
             return list;
-        }
+        };
 
         addCurrentPosition = function (item, currentPosition, list) {
             var arrTemp = [];
-            for(var i =list.length - 1; i>= currentPosition; i++){
+
+            var i = currentPosition;
+            var size = list.length;
+            while(i<size){
                 arrTemp.push(list.pop());
+                i++;
             }
+
             list.push(item);
-            for(var i = arrTemp.length - 1; i>=0; i--){
+
+            i=0;
+            size=arrTemp.length;
+            while(i<size){
                 list.push(arrTemp.pop());
+                i++;
             }
+
             return list;
-        }
+        };
 
         return function (list) {
             var result = [];
+
             if (angular.isArray(list) && list.length > 0) {
                 result.push(list[0]);
                 for (var i = 1; i < list.length; i++) {
-                    for(var j = 0; j<result.length; j++){
-                        if(compareDateTimes(list[i], result[j])){
+                    for (var j = 0; j < result.length; j++) {
+                        if (compareDateTimes(list[i], result[j])) {
                             addCurrentPosition(list[i], j, result);
-                        }
-                        else{
-                            addLastPosition(list[i], result);
+                            break;
                         }
                     }
                 }
