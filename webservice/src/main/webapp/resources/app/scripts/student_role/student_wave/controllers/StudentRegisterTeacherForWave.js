94952ec4705ac6a3f4cdb36e63e62eb5b3ff5900
Lien <lien.hoang@smartdev.vn>
2016-05-10 18:17:25 +0700

    student role: register teacher for wave

--- webservice/src/main/webapp/resources/app/scripts/student_role/student_wave/controllers/StudentRegisterTeacherForWave.js
+++ webservice/src/main/webapp/resources/app/scripts/student_role/student_wave/controllers/StudentRegisterTeacherForWave.js
@@ -1,10 +1,27 @@
 angular.module('appDATN.student_wave')
     .controller('StudentRegisterTeacherForWave', function ($scope, $state, StudentWaveService, user) {
 
+        $scope.joinedWaves;
+        $scope.joiningWaves;
         $scope.studentId = user.id;
 
-        function load() {
+        $scope.getWavesStudentJoined = function () {
+            StudentWaveService.getWavesStudentJoined($scope.studentId)
+                .success(function (data) {
+                    if (data.headers.resultCode == 0) {
+                        $scope.joinedWaves = data.body.projectWavesJoined;
+                        $scope.joiningWaves = data.body.projectWavesJoining;
+                    }else{
+                        $state.go('error');
+                    }
+                })
+                .error(function (error) {
+                    $state.go('error');
+                });
+        };
 
+        function load() {
+            $scope.getWavesStudentJoined();
         }
 
         load();
--- webservice/src/main/webapp/resources/app/scripts/student_role/student_wave/student_wave_main.js
+++ webservice/src/main/webapp/resources/app/scripts/student_role/student_wave/student_wave_main.js
@@ -3,11 +3,12 @@
 angular.module('appDATN.student_wave')
     .config(function ($stateProvider) {
         $stateProvider
-            .state('student_role.waves_student_joined', {
+            .state('student_role.waves_student', {
                 parent: 'student_role',
                 url: '/wave/detail',
                 resolve:{
                     user: function ($q, $state, AuthService) {
+                        console.log("user");
                         var deferred = $q.defer();
                         AuthService.getSession()
                             .success(function (data) {
@@ -29,35 +30,23 @@ angular.module('appDATN.student_wave')
                 },
                 views: {
                     content_view: {
+                        template: '<div ui-view="content_view"></div>'
+                    }
+                }
+            })
+            .state('student_role.waves_student_joined', {
+                parent: 'student_role.waves_student',
+                url: '/wave/detail',
+                views: {
+                    content_view: {
                         controller:'InfoWavesStudentJoinedCtrl',
                         templateUrl: '/resources/app/scripts/student_role/student_wave/views/info_waves_student_join.html'
                     }
                 }
             })
             .state('student_role.student_register_teacher_for_wave', {
-                parent: 'student_role',
+                parent: 'student_role.waves_student',
                 url: '/wave/registerTeacher',
-                resolve:{
-                    user: function ($q, $state, AuthService) {
-                        var deferred = $q.defer();
-                        AuthService.getSession()
-                            .success(function (data) {
-                                if(data.headers.resultCode == 0) {
-                                    if(data.body == null){
-                                        $state.go('login');
-                                    }else{
-                                        deferred.resolve(data.body);
-                                    }
-                                }else{
-                                    $state.go('login');
-                                }
-                            })
-                            .error(function (error) {
-                                deferred.reject('Error');
-                            });
-                        return deferred.promise;
-                    }
-                },
                 views: {
                     content_view: {
                         controller:'StudentRegisterTeacherForWave',
